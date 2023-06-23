ea63796dfb762a6f6408a2d03ed17160ba97b7b9
Hugoxoft <56564377+hugoxoft@users.noreply.github.com>
2023-06-23 10:57:38 -0600

    Add files via upload

---
+++ envioemail.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="envioemail.aspx.vb" Inherits="proyectos_varios.envioemail" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Envió de email</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" Text="eMail destino: "></asp:Label>
+            <asp:TextBox ID="TextBox_destino" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label runat="server" Text="Titulo: "></asp:Label>
+            <asp:TextBox ID="TextBox_titulo" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label runat="server" Text="Contenido: "></asp:Label>
+            <asp:TextBox ID="TextBox_contenido" runat="server" TextMode="MultiLine"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button_enviar" runat="server" Text="Enviar" OnClick="Button_enviar_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ envioemail.aspx.vb
@@ -0,0 +1,29 @@
+﻿
+Public Class envioemail
+    Inherits System.Web.UI.Page
+    Protected Sub Button_enviar_Click(sender As Object, e As EventArgs)
+        Try
+            Dim usuario As String = "hugoxoft@hotmail.com"
+            Dim contrasena As String = "xsmtpsib-b8ab6b65cefd87880f9298cf5d9f8e4653779329418bfe0425c8044f1911978e-yOFxfR1stjH9KNIw"
+
+            Dim correo As New System.Net.Mail.MailMessage
+            correo.To.Add(Me.TextBox_destino.Text)
+            correo.From = New System.Net.Mail.MailAddress(usuario)
+            'correo.CC.Add("")
+            'correo.Bcc.Add("")
+            correo.Subject = Me.TextBox_titulo.Text
+            correo.Body = "<br>" & "<strong>" + Me.TextBox_contenido.Text + "</strong>"
+            correo.IsBodyHtml = True
+            correo.Priority = System.Net.Mail.MailPriority.Normal
+            Dim smtp As New System.Net.Mail.SmtpClient("smtp-relay.sendinblue.com")
+            smtp.UseDefaultCredentials = False
+            smtp.Port = 587
+            smtp.Credentials = New System.Net.NetworkCredential(usuario, contrasena)
+            smtp.Send(correo)
+            ScriptManager.RegisterClientScriptBlock(Me, Me.GetType(), "alertMessage", "alert('Se ha enviado el email')", True)
+        Catch ex As Exception
+            ScriptManager.RegisterClientScriptBlock(Me, Me.GetType(), "alertMessage", "alert('" + ex.Message + "')", True)
+        End Try
+    End Sub
+
+End Class
